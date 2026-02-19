5e8ae0290158c10e9e6db75d344dbc09dda0558c
Sohan Shrestha <itsmesohan456@gmail.com>
2026-02-19 20:11:43 +0545

    Finished doing the database setup and core game function

--- RockPaperScissorsGame..py/RockPaperScissorsGame.py
+++ RockPaperScissorsGame..py/RockPaperScissorsGame.py
@@ -2,4 +2,27 @@ import random
 import sqlite3
 
 conct = sqlite3.connect('game_results.db')
+cursor = conct.cursor()
 
+cursor.execute('''
+    CREATE TABLE IF NOT EXIST stats ( 
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               user_choice TEXT,
+               computer_choice TEXT,
+               result TEXT
+    )
+''')
+conct.commit()
+
+def play_game():
+    options = ["rock", "paper", "scissors"]
+
+    print("--- Welcome to Rock, Paper, Scissors! ---")
+    user_choice = input("Enter your choice from Rock, Paper, or Scissors  (or quit to stop): ").lower()
+
+    if user_choice == 'quit':
+        return False
+    
+    if user_choice not in options:
+        print("Oops!!\nInvalid move!!\nTry again")
+        return True

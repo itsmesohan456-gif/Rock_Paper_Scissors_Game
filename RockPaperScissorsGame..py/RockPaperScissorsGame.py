a4dd37047cc15e938e3129a0b1ad35558f40aa68
Sohan Shrestha <itsmesohan456@gmail.com>
2026-02-20 21:31:02 +0545

    Added Win/Lose Logic

--- RockPaperScissorsGame..py/RockPaperScissorsGame.py
+++ RockPaperScissorsGame..py/RockPaperScissorsGame.py
@@ -18,7 +18,8 @@ def play_game():
     options = ["rock", "paper", "scissors"]
 
     print("--- Welcome to Rock, Paper, Scissors! ---")
-    user_choice = input("Enter your choice from Rock, Paper, or Scissors  (or quit to stop): ").lower()
+    
+    user_choice = input("Enter your choice from Rock, Paper, or Scissors (or quit to stop): ").lower()
 
     if user_choice == 'quit':
         return False
@@ -26,3 +27,22 @@ def play_game():
     if user_choice not in options:
         print("Oops!!\nInvalid move!!\nTry again")
         return True
+    
+    computer_choice = random.choice(options)
+    print(f"Computer chose: {computer_choice}")
+
+    if user_choice == computer_choice:
+        result = "Draw"
+    elif (user_choice == "rock" and 
+    computer_choice == "scissors") or \
+             (user_choice == "paper" and 
+    computer_choice == "rock") or \
+             (user_choice == "scissors" and 
+    computer_choice == "paper"):
+        result = "You Win!"
+    else:
+        result = "You Lose!"
+
+    print(f"Result: {result}")
+
+                 
